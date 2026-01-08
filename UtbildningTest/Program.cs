d7d5b2be74116e684fecc1cbc39f51bc7796738e
Philip Oxelius <philip.oxelius@gmail.com>
2026-01-08 10:55:12 +0100

    Added some functionallity

--- UtbildningTest/Program.cs
+++ UtbildningTest/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+Enumerable.Range(1, 5)
+    .ToList()
+    .ForEach(x => Console.WriteLine("Hello, World!"));
